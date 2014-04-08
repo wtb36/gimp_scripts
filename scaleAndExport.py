bef64b668408c88f340c30cd955bd32affe991c2
wtb36 <wtb36.users.noreply.github.com>
2014-04-08 16:11:21 +0200

    Tidy up.

--- scaleAndExport.py
+++ scaleAndExport.py
@@ -4,20 +4,18 @@ import os
 import sys
 
 def resize_and_export(filename, size):
+    # Remove extension (.xcf).
     fn = filename[0:-4]
     pdb.gimp_message(fn)
+    # Open file.
     img = pdb.gimp_file_load(filename, filename, run_mode=RUN_NONINTERACTIVE)
-    #pdb.gimp_message(img.layers)
+    # Get first layer.
     ly = img.layers[0]
-    #pdb.gimp_by_color_select(ly, gimpcolor.RGB(254, 254, 254), 0, CHANNEL_OP_REPLACE, False, False, 0, False)
-    #pdb.gimp_edit_clear(ly)
-    xcffile = "%s_%s.xcf" % (fn, size)
-    #pdb.gimp_file_save(img, ly, xcffile, xcffile)
-
+    # Scale image to size x size.
     pdb.gimp_image_scale(img, size, size)
-    #xcffile = "%s_%s.xcf" % (fn, size)
-    #pdb.gimp_file_save(img, ly, xcffile, xcffile)
+    # Merge visible layers.
     newlayer = img.merge_visible_layers(0)
+    # Save as png.
     pngfile = "%s_%s.png" % (fn, size)
     pdb.file_png_save(img, newlayer, pngfile, pngfile, 1, 0, 0, 0, 0, 0, 0)
 
